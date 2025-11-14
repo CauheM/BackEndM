80e072575923589bcf180748e5195169e2c2fcc5
Cauhê <cauhesanchesmatheus@hotmail.com>
2025-11-14 16:29:58 -0300

    atividades realizadas

---
+++ POO/PilaresPoo/Polimorfismo/ExercicioPolimorfismo/Program.cs
@@ -0,0 +1,17 @@
+﻿using ExercicioPolimorfismo;
+
+PagamentoPix comprapix = new PagamentoPix();
+comprapix.ValorCompra = 1500;
+float valorpagar = comprapix.CalcularTotal();
+Console.WriteLine($"Celular");
+Console.WriteLine($"Preço: {comprapix.ValorCompra}");
+Console.WriteLine($"Pagamento com desconto: {valorpagar}");
+
+PagamentoCartao compracartao = new PagamentoCartao();
+compracartao.ValorCompra = 60;
+
+Console.WriteLine($"Caneco");
+Console.WriteLine($"Preço: {compracartao.ValorCompra}");
+Console.WriteLine($"Tipo de pagamento: Cartão");
+Console.WriteLine($"Pagamento com desconto: {compracartao.CalcularTotal()}");
+
